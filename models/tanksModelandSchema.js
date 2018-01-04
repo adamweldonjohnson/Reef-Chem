5adbbc4d8b0aec99652cd82d23a78cd61d6c14ee
awj03130 <adamweldonjohnson@gmail.com>
2018-01-04 11:19:33 -0600

    Added all tests to testSchema. Need to revise values, but template for all required tests has been updated. Also added tankSchema detail and Commentary. TODOs in both files.

--- models/tanksModelandSchema.js
+++ models/tanksModelandSchema.js
@@ -1,11 +1,45 @@
 'use strict';
-
+// Require statements
 const mongoose = require('mongoose');
-
+// Setting up schema and storing as variable
 let tanksSchema = mongoose.Schema({
-
+  // Storing the name of the tanks
+  name: {
+    // Stored as a string, disallows numbers
+    type: String,
+    // Requires a name be entered for a tank - this will be displayed on the frontend
+    required: true,
+    // Prevents erroneous characters
+    trim: true,
+    // Requires a minimum length of two characters in the name, allows for names such as 'Po', but not single letters
+    minLength: 2
+    // Provides default name for display purposes (which the user will be able to later edit) in case the user does not choose to input a value.
+    default: 'My Aquarium'
+  },
+  // Storing the size of the tank
+  gallons: {
+    // Only allows numbers, in case tank sizes are later needed for computation. Could possibly be selected from a dropdown selector on the front end.
+    type: Number,
+    // This is not required
+    required: false,
+    // This is trimmed
+    trim: true,
+    // Allows for very small aquariums, e.g. 3 gal reef tanks
+    minLength: 1
+  },
+  // Storing the user ID for association with test data.
+  userId: {
+    // TODO: find out if type needs to be mixed or objectID. Unclear if OBJID is valid.
+    type: Mixed,
+    // Required to allow association of test data with specific tank and user
+    required: true,
+    // Trimmed
+    trim: true,
+    // TODO: Find out if required or recommended.
+    minLength: 20,
+  }
 })
-
+// Setting 'Test' model using tankSchema
 let Tanks = mongoose.model('Tanks', tanksSchema);
-
+// Exporting for use in server.js
 module.exports = Tanks;
--- models/testModelAndSchema.js
+++ models/testModelAndSchema.js
@@ -1,11 +1,139 @@
 'use strict';
-
+// Require statements
 const mongoose = require('mongoose');
-
+// Setting up schema and storing as variable
 let testSchema = mongoose.Schema({
-
+  pH: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Alkalinity - may seem like duplicate of pH, but measures buttering ability of water.
+  Alk: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  //Ammonia
+  Ammonia: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  //Calcium
+  Ca: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Nitrite
+  NO2: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Nitrate
+  NO3: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Phosphate
+  PO: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Salinity
+  S: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Specific Gravity
+  SG: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Magnesium
+  Mg: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Iodine
+  I: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Silica
+  SiO2: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Stontium
+  Sr: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  //Boron
+  B: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  //Iron
+  Fe: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  // Temperature
+  Temp: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  date: {
+    type: Date,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  userId: {
+    type: Number,
+    required: false,
+    trim: true,
+    minLength: 1
+  },
+  tankId: {
+    type: Mixed, // TODO: find out if  tank id is mixed
+    required: true,
+    trim: true,
+    minLength: 1 // TODO: find out length of tankId
+  }
 })
-
+// Setting 'Test' model using testSchema
 let Test = mongoose.model('Test', testSchema);
-
+// Exporting for use in server.js
 module.exports = Test;
