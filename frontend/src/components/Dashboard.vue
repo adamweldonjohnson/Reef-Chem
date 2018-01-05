85070a1acb843a46c4b03848e6771a047e682eb4
Adam Johnson <adamweldonjohnson@gmail.com>
2018-01-05 11:41:09 -0600

    Added Chart.js and navbar.

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,7 +1,44 @@
 <template>
 <div>
-  <h1 class="is-size-1">Dashboard</h1>
-  <a class="button is-danger is-pulled-right" @click="logOut">Log out</a>
+  <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+    </a>
+      <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <div class="field is-grouped">
+            <router-link class="rlink" :to="{ name: 'Home' }">Home</router-link>
+          </div>
+        </div>
+        <div class="navbar-item">
+          <router-link class="rlink" :to="{ name: 'Profile' }">Profile</router-link>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="field is-grouped">
+
+            <p class="control">
+              <a class="button is-danger" @click="logOut">
+              <span>Log out</span>
+            </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <a class="button is-danger is-pulled-right">Log out</a>
   <div class="card">
     <div class="card-image">
       <figure class="image is-4by3">
@@ -34,6 +71,7 @@
 
 <script>
 import firebase from 'firebase'
+import chart from 'chart.js'
 export default {
   data() {
     return {
@@ -64,6 +102,14 @@ export default {
 <style lang="scss" scoped>
 .card {
     max-width: 35%;
-    margin: auto;
+    margin: 1rem auto;
+}
+
+.rlink {
+    color: white;
+}
+
+.rlink:hover {
+    color: #35dcf2;
 }
 </style>
